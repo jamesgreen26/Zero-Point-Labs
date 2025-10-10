e43b055d4ae8f44a2bcfda45d15fe6564ccb417a
James Green <jgreenop22@gmail.com>
2025-10-10 00:19:10 -0700

    limit speed when not mini scale cuz of mojank

--- src/main/java/g_mungus/zpl/block/launcher/EnergyOrbLauncherBlockEntity.java
+++ src/main/java/g_mungus/zpl/block/launcher/EnergyOrbLauncherBlockEntity.java
@@ -46,7 +46,13 @@ public class EnergyOrbLauncherBlockEntity extends BlockEntity {
         Vec3 blockCenter = Vec3.atCenterOf(pos);
         Vec3 spawnPos = blockCenter.add(direction.scale(0.6));
 
-        Vec3 velocity = direction.scale(7.0);
+        Vec3 velocity;
+
+        if (scale < 0.5) {
+            velocity = direction.scale(7.0);
+        } else {
+            velocity = direction.scale(3.5);
+        }
 
         EnergyOrbEntity energyOrb;
 
