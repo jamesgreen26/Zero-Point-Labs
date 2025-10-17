0ff011253ebee4e297b996afb7eadba49d8bfa72
James Green <jgreenop22@gmail.com>
2025-10-16 20:28:51 -0700

    reorder creative tab

--- src/main/java/g_mungus/zpl/item/ModItems.java
+++ src/main/java/g_mungus/zpl/item/ModItems.java
@@ -22,8 +22,11 @@ public class ModItems {
     public static final RegistryObject<Item> GYROSCOPE_ITEM = ITEMS.register("gyroscope",
             () -> new BlockItem(ModBlocks.GYROSCOPE_BLOCK.get(), new Item.Properties()));
 
-    public static final RegistryObject<Item> CAUTION_BLOCK_ITEM = ITEMS.register("caution_block",
-            () -> new BlockItem(ModBlocks.CAUTION_BLOCK.get(), new Item.Properties()));
+    public static final RegistryObject<Item> MASS_SUSPENSION_MATRIX_ITEM = ITEMS.register("mass_suspension_matrix",
+            () -> new BlockItem(ModBlocks.MASS_SUSPENSION_MATRIX_BLOCK.get(), new Item.Properties()));
+
+    public static final RegistryObject<Item> ENERGY_ORB_LAUNCHER_ITEM = ITEMS.register("photon_pulse_blaster",
+            () -> new BlockItem(ModBlocks.ENERGY_ORB_LAUNCHER.get(), new Item.Properties()));
 
     public static final RegistryObject<Item> SPACE_PLATING_ITEM = ITEMS.register("space_plating",
             () -> new BlockItem(ModBlocks.SPACE_PLATING.get(), new Item.Properties()));
@@ -34,11 +37,8 @@ public class ModItems {
     public static final RegistryObject<Item> SPACE_PLATING_STAIRS_ITEM = ITEMS.register("space_plating_stairs",
             () -> new BlockItem(ModBlocks.SPACE_PLATING_STAIRS.get(), new Item.Properties()));
 
-    public static final RegistryObject<Item> MASS_SUSPENSION_MATRIX_ITEM = ITEMS.register("mass_suspension_matrix",
-            () -> new BlockItem(ModBlocks.MASS_SUSPENSION_MATRIX_BLOCK.get(), new Item.Properties()));
-
-    public static final RegistryObject<Item> ENERGY_ORB_LAUNCHER_ITEM = ITEMS.register("photon_pulse_blaster",
-            () -> new BlockItem(ModBlocks.ENERGY_ORB_LAUNCHER.get(), new Item.Properties()));
+    public static final RegistryObject<Item> CAUTION_BLOCK_ITEM = ITEMS.register("caution_block",
+            () -> new BlockItem(ModBlocks.CAUTION_BLOCK.get(), new Item.Properties()));
 
     public static final RegistryObject<Item> LAUNCH_PLATFORM_ITEM = ITEMS.register("launch_platform",
             () -> new BlockItem(ModBlocks.LAUNCH_PLATFORM.get(), new Item.Properties()));
