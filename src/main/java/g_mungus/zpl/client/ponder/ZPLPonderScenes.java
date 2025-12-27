e0e19af238d8836efab4d1913e8f23f6dc510848
James Green <jgreenop22@gmail.com>
2025-12-27 13:20:29 -0800

    thruster ponder

--- src/main/java/g_mungus/zpl/client/ponder/ZPLPonderScenes.java
+++ src/main/java/g_mungus/zpl/client/ponder/ZPLPonderScenes.java
@@ -1,13 +1,22 @@
 package g_mungus.zpl.client.ponder;
 
+import g_mungus.zpl.block.ModBlocks;
+import g_mungus.zpl.block.thruster.IonModulatorBlock;
+import g_mungus.zpl.block.thruster.ThrusterExhaustBlock;
 import net.createmod.ponder.api.element.ElementLink;
 import net.createmod.ponder.api.element.WorldSectionElement;
 import net.createmod.ponder.api.scene.SceneBuilder;
 import net.createmod.ponder.api.scene.SceneBuildingUtil;
 import net.createmod.ponder.api.scene.Selection;
 import net.createmod.ponder.foundation.instruction.DisplayWorldSectionInstruction;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LeverBlock;
+import net.minecraft.world.level.block.RedStoneWireBlock;
+import net.minecraft.world.level.block.state.properties.AttachFace;
+import net.minecraft.world.level.block.state.properties.RedstoneSide;
 import net.minecraft.world.phys.Vec3;
 
 public class ZPLPonderScenes {
@@ -48,18 +57,60 @@ public class ZPLPonderScenes {
     public static void thrusterTutorial(SceneBuilder builder, SceneBuildingUtil util) {
         int size = 13;
         builder.configureBasePlate(0, 0, size);
+        builder.setSceneOffsetY(-1.5f);
         builder.title("thruster", "Ion Thrusters");
 
-        Selection floor = util.select().fromTo(0, 0, 0, 12, 0, 12);
+        Selection floor = util.select().fromTo(0, 0, 0, 12, 1, 12);
         builder.removeShadow();
-        builder.overlay().showText(55).text("Ion Thrusters can be used to propel a ship.");
 
         Selection ship = util.select().everywhere().substract(floor);
-        builder.addInstruction(new DisplayWorldSectionInstruction(0, Direction.UP, ship, builder.getScene()::getBaseWorldSection));
-        ElementLink<WorldSectionElement> shipElement = builder.world().makeSectionIndependent(ship);
 
-        for (int i = 0; i < 180; i++) {
+        builder.world().showSection(ship, Direction.EAST);
+
+        for (int i = 0; i < 80; i++) {
+            if (i == 10) {
+                builder.overlay().showText(55).text("Ion Thrusters can be used to propel a ship.");
+            }
             particle(builder);
+            builder.idle(1);
+        }
+
+        builder.world().hideSection(ship, Direction.WEST);
+        builder.idle(20);
+
+        builder.world().showSection(floor, Direction.UP);
+        builder.idle(10);
+
+        builder.world().setBlock(new BlockPos(6, 1, 4), ModBlocks.ION_MODULATOR_BLOCK.get().defaultBlockState().setValue(IonModulatorBlock.FACING, Direction.NORTH), true);
+
+        builder.idle(10);
+        builder.world().setBlock(new BlockPos(6, 1, 3), ModBlocks.THRUSTER_EXHAUST_BLOCK.get().defaultBlockState().setValue(ThrusterExhaustBlock.FACING, Direction.NORTH), true);
+        builder.idle(10);
+
+        builder.overlay().showText(75).text("To set one up, place an Ion Thrust Modulator in front of an Ion Thruster Exhaust.");
+
+        builder.idle(80);
+
+        builder.overlay().showText(60).text("Then supply FE and a Redstone Signal to the Ion Thrust Modulator block.");
+
+        builder.idle(10);
+
+        builder.effects().emitParticles(new Vec3(6.5, 2.5, 4.5), builder.effects().simpleParticleEmitter(ParticleTypes.ELECTRIC_SPARK, new Vec3(0, -1, 0)), 2, 20);
+
+        builder.idle(20);
+
+        builder.world().setBlock(new BlockPos(5, 1, 4), Blocks.REDSTONE_WIRE.defaultBlockState().setValue(RedStoneWireBlock.EAST, RedstoneSide.SIDE).setValue(RedStoneWireBlock.WEST, RedstoneSide.SIDE), true);
+
+        builder.idle(5);
+
+        builder.world().setBlock(new BlockPos(4, 1, 4), Blocks.LEVER.defaultBlockState().setValue(LeverBlock.FACE, AttachFace.FLOOR), true);
+
+        builder.idle(10);
+        builder.world().toggleRedstonePower(util.select().position(5, 1, 4));
+        builder.world().toggleRedstonePower(util.select().position(4, 1, 4));
+
+        for (int i = 0; i < 16; i++) {
+            builder.world().setBlock(new BlockPos(6, 1, 3), ModBlocks.THRUSTER_EXHAUST_BLOCK.get().defaultBlockState().setValue(ThrusterExhaustBlock.FACING, Direction.NORTH).setValue(ThrusterExhaustBlock.POWER, i), false);
             builder.idle(1);
         }
 
