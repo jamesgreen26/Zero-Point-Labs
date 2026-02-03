38e7d1b313020679ff305d51baa8c012bd6bec7b
James Green <jgreenop22@gmail.com>
2026-02-02 21:04:23 -0800

    re-add old rendering code

--- src/main/java/g_mungus/zpl/client/ZeroPointLabsModClient.java
+++ src/main/java/g_mungus/zpl/client/ZeroPointLabsModClient.java
@@ -1,5 +1,6 @@
 package g_mungus.zpl.client;
 
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import dev.engine_room.flywheel.api.visual.BlockEntityVisual;
 import dev.engine_room.flywheel.api.visualization.BlockEntityVisualizer;
 import dev.engine_room.flywheel.api.visualization.VisualizationContext;
@@ -10,26 +11,39 @@ import g_mungus.zpl.block.thruster.ThrusterExhaustBlockEntity;
 import g_mungus.zpl.block.ModBlockEntities;
 import g_mungus.zpl.client.flywheel.DummyBERenderer;
 import g_mungus.zpl.client.flywheel.ThrusterVisual;
+import g_mungus.zpl.client.thruster.ThrusterExhaustBlockEntityRenderer;
 import g_mungus.zpl.entity.EnergyOrbEntityRenderer;
 import g_mungus.zpl.entity.ModEntities;
 import g_mungus.zpl.client.particle.EnergyOrbParticle;
 import g_mungus.zpl.client.particle.ModParticles;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.RegisterParticleProvidersEvent;
+import net.minecraftforge.client.event.RegisterShadersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import team.lodestar.lodestone.systems.rendering.shader.ShaderHolder;
+
+import static team.lodestar.lodestone.registry.client.LodestoneShaderRegistry.registerShader;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = ZeroPointLabsMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ZeroPointLabsModClient {
 
+    public static final ShaderHolder THRUST_SHADER = new ShaderHolder(ResourceLocation.fromNamespaceAndPath(ZeroPointLabsMod.MOD_ID, "thrust"), DefaultVertexFormat.POSITION_TEX);
+
+    @SubscribeEvent
+    public static void shaderRegistry(RegisterShadersEvent event) {
+        registerShader(event, THRUST_SHADER);
+    }
+
 
     @SubscribeEvent
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
-        event.registerBlockEntityRenderer(ModBlockEntities.THRUSTER_EXHAUST_BLOCK_ENTITY.get(), DummyBERenderer::new);
+        event.registerBlockEntityRenderer(ModBlockEntities.THRUSTER_EXHAUST_BLOCK_ENTITY.get(), ThrusterExhaustBlockEntityRenderer::new);
         event.registerEntityRenderer(ModEntities.ENERGY_ORB.get(), EnergyOrbEntityRenderer::new);
     }
 
@@ -39,6 +53,7 @@ public class ZeroPointLabsModClient {
     }
 
     @SubscribeEvent
+    @SuppressWarnings("removal")
     public static void onClientSetup(FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
             VisualizerRegistry.setVisualizer(
